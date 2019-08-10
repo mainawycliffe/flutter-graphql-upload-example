566dc5414917d5eddbf4dbd94aa31d956090a4ee
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-10 05:42:00 +0300

    randomize filename when saving

--- backend/resolver.go
+++ backend/resolver.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/99designs/gqlgen/graphql"
@@ -23,7 +25,11 @@ func (r *Resolver) Query() QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) Upload(ctx context.Context, input graphql.Upload) (string, error) {
-	f, err := os.Create(fmt.Sprintf("temp/%s", input.Filename))
+
+	ext := filepath.Ext(input.Filename)
+	filename := fmt.Sprintf("%s%s", randString(10), ext)
+
+	f, err := os.Create(fmt.Sprintf("temp/%s", filename))
 
 	if err != nil {
 		log.Printf("Error: %v", err)
@@ -46,11 +52,21 @@ func (r *mutationResolver) Upload(ctx context.Context, input graphql.Upload) (st
 
 	time.Sleep(2 * time.Second)
 
-	return fmt.Sprintf("http://localhost:8080/temp/%s", input.Filename), nil
+	return fmt.Sprintf("http://localhost:8080/temp/%s", filename), nil
 }
 
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Hello(ctx context.Context, name string) (string, error) {
 	return fmt.Sprintf("Hello, %s", name), nil
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
